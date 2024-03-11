46db8c51a09ae1c35dd088201239b88af4fb86a4
MissionXOR <107556308+MissionXOR@users.noreply.github.com>
2024-03-11 22:52:52 +0600

    first commit

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2024 Designed by <a href="https://github.com/MissionXOR" target="_blank" rel="noopener noreferrer">@X-OR</a></p>
+</div>
